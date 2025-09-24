20d6906fb080eea25aedbe78c64bfef80ac43f2f
felopater-adel12 <felopater.sadek@maxab.io>
2025-09-24 10:08:32 +0300

    Add application context configuration and main class for project initialization

---
+++ bean_lifecycle/src/com/example/spring_training/Main.java
@@ -0,0 +1,11 @@
+package com.example.spring_training;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Start Project");
+
+
+
+        System.out.println("End Project");
+    }
+}
